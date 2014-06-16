0d6025a9d71c3ca315eccd5cb92a74267cfe6452
Henrique Ferreira <henriquebf@gmail.com>
2014-06-17 00:12:31 +0200

    Add files to repository

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+server.listen(app.get('port'), function(){
+  console.log('Socket.io server listening on port ' + app.get('port'));
+});
+
+//http.createServer(app).listen(app.get('port'), function(){
+//  console.log('Express server listening on port ' + app.get('port'));
+//});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
